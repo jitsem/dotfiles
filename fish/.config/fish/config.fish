e38095fb2074e48b34516f20b235111ebf83ba61
jitsem <jitse.meulenijzer@gmail.com>
2025-12-15 16:49:33 +0100

    niri

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,13 +13,6 @@ else
 	abbr -a lll 'ls -la'
 end
 
-# Start X at login
-if status is-login
-    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        exec startx -- -keeptty
-    end
-end
-
 # !! and !$ syntax
 function bind_bang
     switch (commandline -t)[-1]
